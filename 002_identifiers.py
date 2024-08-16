dc970619b76b479461016b9e4594cb87f8bfbe3a
Vaibhav vats <vabhavvats30@gmail.com>
2024-08-16 23:59:47 +0530

    Identifiers

---
+++ 002_identifiers.py
@@ -0,0 +1,11 @@
+# Rules 
+
+# 1. Use only alpha numeric characters and underscores
+# 2. Never start with the number.
+# 3. Use underscore to serprate two words.
+
+
+name = "Vaibhav Vats"
+age = 22
+role = "MERN Stack Developer"
+this_repo = " This repo basically used to add files and code during my learing process and record it."
