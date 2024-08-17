ef6ffc42a1ba65672bbb2abdb2e25927bd69d036
Vaibhav vats <vabhavvats30@gmail.com>
2024-08-17 09:41:53 +0530

    loops in python

---
+++ 007_loops.py
@@ -0,0 +1,10 @@
+# used when we know the iteration
+for v in range(11):
+    print (v*v, end=", ")
+
+
+# used when we don't know the iteration (run until condition is false)
+n = 10
+while n>= 0:
+    print(n)
+    n -=1
